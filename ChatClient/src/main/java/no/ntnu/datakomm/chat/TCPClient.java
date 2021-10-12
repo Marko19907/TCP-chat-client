4536fedc627ac59a8bfdfb1339dcdcf973034ba1
Kasper Nilssen <kasper.nilssen1@gmail.com>
2021-10-12 14:26:49 +0200

    remove useless commnet

--- ChatClient/src/main/java/no/ntnu/datakomm/chat/TCPClient.java
+++ ChatClient/src/main/java/no/ntnu/datakomm/chat/TCPClient.java
@@ -75,7 +75,7 @@ public class TCPClient {
         }
 
         this.toServer.println(cmd);
-
+        System.out.println("Sent command: " + cmd);
         return true;
     }
 
@@ -91,7 +91,6 @@ public class TCPClient {
             this.lastError = "Could not send the message";
             return false;
         }
-        // TODO: Can a String be blank, not empty?
         return this.sendCommand("msg " + message);
     }
 
